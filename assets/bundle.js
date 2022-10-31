93f11d5064715252094812b50d04e441865aed66
Kevin Papst <kpapst@gmx.net>
2022-10-31 15:02:52 +0100

    new tabler design, build assets with webpack

---
+++ assets/bundle.js
@@ -0,0 +1,15 @@
+require ('/assets/bundle.scss')
+
+//import '@tabler/core/src/js/src/autosize';
+//import '@tabler/core/src/js/src/input-mask';
+import '@tabler/core/src/js/src/dropdown';
+import '@tabler/core/src/js/src/tooltip';
+import '@tabler/core/src/js/src/popover';
+//import '@tabler/core/src/js/src/switch-icon';
+//import '@tabler/core/src/js/src/tab';
+//import '@tabler/core/src/js/src/toast';
+import * as bootstrap from 'bootstrap';
+import * as tabler from '@tabler/core/src/js/src/tabler';
+
+globalThis.bootstrap = bootstrap;
+globalThis.tabler = tabler;
