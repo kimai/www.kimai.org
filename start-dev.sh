5f8d4c9739309159865766b5f5dbd669aeca6eef
Kevin Papst <kpapst@gmx.net>
2024-03-18 11:24:12 +0100

    jekyll environment check

--- start-dev.sh
+++ start-dev.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll serve -H 0.0.0.0 --incremental --config _config.yml,_config.dev.yml
+JEKYLL_ENV=development bundle exec jekyll serve -H 0.0.0.0 --incremental --config _config.yml,_config.dev.yml
