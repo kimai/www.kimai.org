9ba88178b9881bf78d682e46233dad4bd0f1382e
Kevin Papst <kpapst@gmx.net>
2024-07-29 12:08:36 +0200

    added alert plugin

---
+++ _plugins/alert.rb
@@ -0,0 +1,32 @@
+module Jekyll
+    # {% alert %} Some info message {% endalert %}
+    # {% alert info|warning|danger %} Well done! You successfully read this important alert message. {% endalert %}
+
+    class AlertBlock < Liquid::Block
+
+        def initialize(tag_name, markup, tokens)
+            @style = "info"
+            @icon = "message-rounded-detail"
+            if /(?<style>[^\s]+)/i =~ markup
+                if style == 'warning'
+                    @icon = 'alarm-exclamation'
+                    @style = style
+                elsif style == 'danger'
+                    @icon = 'info-circle'
+                    @style = style
+                end
+            end
+            super
+        end
+
+        def render(context)
+            site      = context.registers[:site]
+            converter = site.find_converter_instance(Jekyll::Converters::Markdown)
+            output = converter.convert(super(context))
+            return "<div class=\"alert alert-#{@style} d-flex\" role=\"alert\"><i class=\"bx bx-#{@icon} lead me-3\"></i>#{output}</div>"
+        end
+    end
+
+end
+
+Liquid::Template.register_tag('alert', Jekyll::AlertBlock)
